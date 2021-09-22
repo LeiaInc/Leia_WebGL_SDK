85f54451ac4f404d55c663822bbe6485770b959f
JoeLang_LeiaInc <joseph.lang@leiainc.com>
2021-09-22 16:10:00 -0400

    Remove Seven Summits reference

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,7 +1,4 @@
 const mix = require('laravel-mix');
-// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-// const CompressionPlugin = require('compression-webpack-plugin');
-// require('laravel-mix-ejs');
 
 if (!mix.inProduction()) {
     mix
@@ -10,17 +7,8 @@ if (!mix.inProduction()) {
 
         // bundles js: (from, to)
         .js('example0/main.js', 'dist/example0.js')
-        .js('example1/main.js', 'dist/example1.js')
         .sass('example1/index.scss', 'dist/example1.css')
 
-        // compiles sass and add css3 prefixes: (from, to)
-        // .sass('src/styles/index.scss', 'dist/')
-
-        // compiles ejs templates: (from, to, contents, options)
-        // .ejs('src/views', 'dist', contents, { rmWhitespace: true, partials: 'src/views/partials' })
-
-        // serves assets and sync with browser sync
-        // .browserSync({ server: 'dist', proxy: null })
 } else {
     mix
         // sets public path for manifest file
@@ -28,13 +16,4 @@ if (!mix.inProduction()) {
 
         // bundles js: (from, to)
         .js('main.js', 'dist/')
-
-        // compiles sass and add css3 prefixes: (from, to)
-        // .sass('src/styles/index.scss', 'dist/')
-
-        // compiles ejs templates: (from, to, contents, options)
-        // .ejs('src/views', 'dist', contents, { rmWhitespace: true, partials: 'src/views/partials' })
-
-        // cleans dist directory and compresses assets
-        // .webpackConfig({ plugins: [ new CleanWebpackPlugin(), new CompressionPlugin() ] })
 }
